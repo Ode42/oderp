37012559a320188ae0215a55c542228d72e0d959
Ode42 <ode.progr@outlook.com>
2022-01-14 20:32:57 +0200

    App frontend setup and connection to backend service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [message, setMessage] = useState([]);
+  fetch("http://localhost:5000")
+    .then(response => response.json())
+    .then(data => {
+      setMessage(data)
+    })
+    .catch(error => console.error(error))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message.message}</h1>
     </div>
   );
 }

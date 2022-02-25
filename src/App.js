42919cd96b6756148f20a16d35b2ecc6ada0a84d
Ode42 <ode.progr@outlook.com>
2022-02-25 19:05:20 +0200

    useEffect hook for fetching test data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './styles/App.css';
-import React, {useState, useEffect} from "react";
+import "./styles/App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [message, setMessage] = useState([]);
-  fetch("http://localhost:5000")
-    .then(response => response.json())
-    .then(data => {
-      setMessage(data)
-    })
-    .catch(error => console.error(error))
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const data = await fetch("http://localhost:5000/api/projects")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setMessage(data);
+        });
+    };
+    fetchMessage().catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <h1>{message.message}</h1>

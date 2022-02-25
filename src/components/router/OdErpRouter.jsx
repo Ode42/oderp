3eca3e5805e42b8351c5f395f9db92c9f6311696
Ode42 <ode.progr@outlook.com>
2022-02-25 19:24:21 +0200

    Home page

--- src/components/router/OdErpRouter.jsx
+++ src/components/router/OdErpRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Projects from "../Projects";
+import Home from "../Home";
 
 const OdErpRouter = () => {
   return (
@@ -8,6 +9,7 @@ const OdErpRouter = () => {
       <Router>
         <Routes>
           <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>

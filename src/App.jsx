14725b7d1ec0c6c828ea5ece22da9d54ec16bd44
Ode42 <ode.progr@outlook.com>
2022-02-27 17:31:15 +0200

    Projects data fetch to the component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,6 @@ import React, { useState, useEffect } from "react";
 import OdErpRouter from "./components/router/OdErpRouter";
 
 function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    const fetchMessage = async () => {
-      const data = await fetch("http://localhost:5000/api/projects")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setMessage(data);
-        });
-    };
-    fetchMessage().catch((error) => console.error(error));
-  }, []);
-
   return (
     <div className="App">
       <OdErpRouter />
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Projects = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const data = await fetch("http://localhost:5000/api/projects")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setMessage(data);
+        });
+    };
+    fetchMessage().catch((error) => console.error(error));
+  }, []);
   return (
     <div>
       <h1>Projects page</h1>
